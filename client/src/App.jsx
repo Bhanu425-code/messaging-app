82ff91851fc5203e041fe6741f3eb04e2bf8cd2d
Bhanu Shree Rekha Gullapalli <bhanu2003gullapalli@gmail.com>
2023-10-10 11:22:09 +0530

    medical-pager

---
+++ client/src/App.jsx
@@ -0,0 +1,72 @@
+import React,{useState} from 'react'
+import {StreamChat} from 'stream-chat';
+import { Chat} from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+
+import { ChannelListContainer,ChannelContainer,Auth } from './components';
+
+import 'stream-chat-react/dist/css/index.css';
+import './App.css';
+
+const cookies=new Cookies();
+//const apiKey='1265840';
+const apiKey='day7c5bz6mnp';
+
+const authToken=cookies.get("token");
+const client=StreamChat.getInstance(apiKey);
+
+if(authToken){
+  client.connectUser({
+  
+    id:cookies.get('userId'),
+    name:cookies.get('username'),
+    fullName:cookies.get('fullName'),
+    image:cookies.get('avatarURL'),
+   
+ 
+    hashedPassword:cookies.get('hashedPassword'),
+    phoneNumner:cookies.get('phoneNumner'),
+
+  },authToken)
+}
+
+const App = () => {
+
+const [createType,setCreateType]=useState('');
+const [isCreating,setIsCreating]=useState(false);
+const [isEditing,setIsEditing]=useState(false);
+
+
+  if(!authToken) return <Auth/>
+  return (
+    
+        <div className='app__wrapper'>
+            <Chat client={client} theme='team light'>
+                <ChannelListContainer
+                isCreating={isCreating}
+                setIsCreating={setIsCreating}
+                setCreateType={setCreateType}
+                setIsEditing={setIsEditing}
+                />
+                <ChannelContainer
+                isCreating={isCreating}
+                setIsCreating={setIsCreating}
+                isEditing={isEditing}
+                setIsEditing={setIsEditing}
+                createType={createType}
+                />
+            </Chat>
+        </div>
+      
+    
+  )
+}
+
+export default App;
+
+
+
+
+
+//const apiKey='1265840';
+//const apiKey='day7c5bz6mnp';
